e8cff8eb6350e1105503e93356bf82c1104a2ad4
Thuc <thuctdt@gmail.com>
2020-02-13 17:07:42 -0500

    Commit for part E

--- src/main/java/com/example/animal/Solution.java
+++ src/main/java/com/example/animal/Solution.java
@@ -1,8 +1,27 @@
 package com.example.animal;
 
+import java.util.Arrays;
+
 public class Solution {
 
 	public static void main(String[] args) {
+		Animal[] animals = new Animal[] {
+				new Duck(),
+				new Chicken(),
+				new Chicken(Animal.Gender.MALE),
+				new Parrot(),
+				new Shark(),
+				new Clownfish(),
+				new Dolphin(),
+				new Butterfly(),
+				new Caterpillar(
+						)
+		};
 		
+		long noOfFlyable = Arrays.stream(animals).filter(n -> n instanceof Flyable).count();
+		long noOfWalkable = Arrays.stream(animals).filter(n -> n instanceof Walkable).count();
+		long noOfSingable = Arrays.stream(animals).filter(n -> n instanceof Singable).count();
+		long noOfSwimable = Arrays.stream(animals).filter(n -> n instanceof Swimmable).count();
+		System.out.println(noOfFlyable+"/"+noOfWalkable+"/"+noOfSingable+"/"+noOfSwimable);
 	}
 }
